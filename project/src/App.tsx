879d38448d0d719bb77ca362e0f87f9aa831b8e2
adeoluwa-adebiyi <adeoluwaadebiyi@gmail.com>
2021-01-11 21:41:20 +0100

    start impl

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,12 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 import SplashScreen from './components/splash-screen';
 
 const  App = (): JSX.Element => {
   return (
-    <div className="App">
+    <Box height="100%">
       <SplashScreen/>
-    </div>
+    </Box>
   );
 }
 
