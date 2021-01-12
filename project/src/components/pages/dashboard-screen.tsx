bfb21c5fa67ccf6dc7f2e4f82c56ef0c9799df5c
adeoluwa-adebiyi <adeoluwaadebiyi@gmail.com>
2021-01-12 09:20:23 +0100

    start impl

--- project/src/components/pages/dashboard-screen.tsx
+++ project/src/components/pages/dashboard-screen.tsx
@@ -1,9 +1,16 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
 
 const DashboardScreen = (): JSX.Element=>{
     return (
-        <Box>
-
+        <Box color="blueviolet.200" height="100%">
+            <Stack height="100%" direction="row">
+                <Stack flex={1} height="100%" direction="column" alignSelf="stretch" justifyContent="center" alignItems="center">
+                    <p>Nav Section</p>
+                </Stack>
+                <Stack flex={1}  height="100%" direction="column" justifyContent="center" alignItems="center">
+                    <p>Router Section</p>
+                </Stack>
+            </Stack>
         </Box>
     );
 }
